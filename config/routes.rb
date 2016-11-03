d8246047a72a2a195b4de9beac328080dca938dd
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-11-03 20:50:04 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => 'posts#index'
+
+  get    'posts',                                                        :to => 'posts#index'
+  get    'posts/:id',                                                    :to => 'posts#show'
+  post   'posts/new',                                                    :to => 'posts#create'
+  get    'posts/:id/edit',                                               :to => 'posts#edit'
+  patch  'posts/:id/edit',                                               :to => 'posts#update'
+  delete 'posts/:id',                                                    :to => 'posts#destroy'
 end
