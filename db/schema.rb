047c5945a8bac8443dac8e023ef8537896011bd0
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-11-03 21:10:06 +0000

    definitely didn't copy this from something else

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103202340) do
+ActiveRecord::Schema.define(version: 20161103202812) do
+
+  create_table "authorized_bots", force: :cascade do |t|
+    t.string   "name"
+    t.string   "key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
