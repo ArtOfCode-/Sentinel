1370467bb158d8cda2d350246583d23ffe9f66f0
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-11-04 22:22:00 +0000

    display post reasons

---
+++ app/controllers/reasons_controller.rb
@@ -0,0 +1,39 @@
+class ReasonsController < ApplicationController
+  before_action :set_reason, :except => [:index]
+
+  def index
+    @reasons = Reason.all.order(:created_at => :desc).paginate(:page => params[:page], :per_page => 50)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @reason.update reason_params
+      redirect_to url_for(:controller => :reasons, :action => :show, :id => @reason.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @reason.destroy
+      flash[:success] = "Removed reason #{@reason.id}."
+      redirect_to url_for(:controller => :reasons, :action => :index)
+    else
+      render :show
+    end
+  end
+
+  private
+  def set_reason
+    @reason = Reason.find params[:id]
+  end
+
+  def reason_params
+    params.require(:reason).permit(:name)
+  end
+end
---
+++ test/controllers/reasons_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ReasonsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
