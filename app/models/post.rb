26e395b8c3323f9142bb36526a30d4f904362213
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-11-05 15:57:40 +0000

    some convoluted mess of a majority verdict

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_and_belongs_to_many :reasons
+  has_many :feedbacks
 
   validates :title, :presence => true
   validates :body, :presence => true
@@ -9,4 +10,11 @@ class Post < ApplicationRecord
   validates :username, :presence => true
   validates :user_reputation, :presence => true
   validates :nato_score, :presence => true, :inclusion => -30..30   # -30..30 as a sanity check rather than a validation
+
+  def majority_feedback
+    Rails.cache.fetch "post_#{self.id}_majority_feedback", :expires_in => 1.hour do
+      # Yes, I did use `find_by :id` deliberately - we *want* `nil` if it can't be found.
+      FeedbackType.find_by id: self.feedbacks.joins(:feedback_type).group('feedback_types.id').order('count(feedbacks.id) desc').count.first.try(:[], 0)
+    end
+  end
 end
