8e13dbe0dfb620e082aafc0cac8bf3b9d2e511f6
ArtOfCode- <owen.jenkins2903@gmail.com>
2017-02-02 00:24:24 +0000

    move cache invalidation to a callback

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,4 +4,9 @@ class Feedback < ApplicationRecord
 
   validates :feedback_type, :presence => true
   validates :post, :presence => true
+
+  after_create do
+    Rails.cache.delete("post_#{self.post.id}_majority_feedback")
+    Feedback.where(:chat_id => self.chat_id, :post_id => self.post_id).where.not(:id => @feedback.id).destroy_all
+  end
 end
