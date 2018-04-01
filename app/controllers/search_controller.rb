fd723d62bb61d9ef8b60aba0d1082ed381684951
Fortunate-MAN <shivashish.ahuja@gmail.com>
2018-04-01 11:54:21 +0530

    Fix #5

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,6 +6,21 @@ class SearchController < ApplicationController
     @results = Post.where('title LIKE ?', "%#{search_term}%")
                .or(Post.where('body LIKE ?', "%#{search_term}%"))
                .or(Post.where('username LIKE ?', "%#{search_term}%"))
+
+    case params[:feedback]
+    when "true positive"
+      feedback = 1
+    when "false positive"
+      feedback = 2
+    when "true negative"
+      feedback = 3
+    when "needs edit"
+      feedback = 4
+    end
+
+    if feedback.present?
+      @results = @results.joins(:feedbacks).where(:feedbacks => {:feedback_type_id => feedback})
+    end    
     @results = @results.order(:created_at => :desc).paginate(:page => params[:page], :per_page => 100)
   end
 end
